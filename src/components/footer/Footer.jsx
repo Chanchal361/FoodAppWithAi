c299acd6c3a231ffd80eb416702d9d563dd3e0f7
Chanchal Kumar <157704288+Chanchal361@users.noreply.github.com>
2024-07-28 10:25:04 +0530

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,9 +16,9 @@ function Footer() {
 Healthy food provides essential nutrients, boosts energy, and supports overall well-being. It includes fruits, vegetables, whole grains, and lean proteins.
             </p>
             <div className="footer_social">
-             <Link to="https://github.com/harshal0151/FoodBookingAI-Recipes-Generator"> <FaGithub/></Link>
-              <Link to= "https://www.linkedin.com/in/harshal-patil-187a87245/"><FaLinkedinIn/></Link>
-              <Link to= "https://portfolio-harshal-jldur6pn7-harshal0151s-projects.vercel.app/" ><ImProfile/></Link>
+             <Link to="https://github.com/Chanchal36"> <FaGithub/></Link>
+              <Link to= "https://leetcode.com/u/chanchalraja/"><FaLinkedinIn/></Link>
+              <Link to= "" ><ImProfile/></Link>
             </div>
           </div>
           <div className="footer_center">
@@ -33,13 +33,13 @@ Healthy food provides essential nutrients, boosts energy, and supports overall w
           <div className="footer_right">
             <h2>Get in touch</h2>
             <ul>
-              <li>+91 7470917488</li>
-              <li>patidaruttam74@gmail.com</li>
+              <li>+91 9117954848</li>
+              <li>chanchalr252@gmail.com</li>
             </ul>
           </div>
         </div>
         <hr/>
-        <p className="footer_copy-right">Copyright 2024 © || Uttam Patidar - All Right Reserved.</p>
+        <p className="footer_copy-right">Copyright 2024 © || Chanchal Raja - All Right Reserved.</p>
         </footer>
     </>
   );
